fe4ba078956e236cccc419802170bbb264a7a2ce
Ross Tollefson <past9sys@gmail.com>
2023-05-13 15:45:53 -0700

    Ast::variables()

--- cas-core/src/polynomial.rs
+++ cas-core/src/polynomial.rs
@@ -1,6 +1,57 @@
+use std::collections::{BTreeSet, HashSet};
+
 use crate::ast::{helpers::int, Ast};
 
 impl Ast {
+    pub fn variables(&self) -> BTreeSet<Ast> {
+        match self {
+            // VAR-1
+            Ast::Und | Ast::Int(_) | Ast::Frc(_) => BTreeSet::new(),
+
+            // VAR-2
+            pow @ Ast::Pow(base, exp) => {
+                if exp.is_int() && **exp > int(1) {
+                    BTreeSet::from_iter(std::iter::once((**base).clone()))
+                } else {
+                    BTreeSet::from_iter(std::iter::once(pow.clone()))
+                }
+            }
+
+            // VAR-3
+            Ast::Sum(operands) => {
+                let mut set = BTreeSet::new();
+
+                for operand in operands.iter() {
+                    set.extend(operand.variables());
+                }
+
+                set
+            }
+
+            // VAR-4
+            Ast::Prd(operands) => {
+                let mut set = BTreeSet::new();
+
+                for operand in operands.iter() {
+                    if operand.is_sum() {
+                        set.insert(operand.clone());
+                    } else {
+                        set.extend(operand.variables());
+                    }
+                }
+
+                set
+            }
+
+            other @ (Ast::Sym(_)
+            | Ast::Neg(_)
+            | Ast::Fac(_)
+            | Ast::Dif(_, _)
+            | Ast::Quo(_, _)
+            | Ast::Fun(_, _)) => BTreeSet::from_iter(std::iter::once(other.clone())),
+        }
+    }
+
     pub fn is_monomial_gpe(&self, variables: &[Ast]) -> bool {
         if variables.contains(self) {
             return true;
@@ -49,6 +100,8 @@ impl Ast {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::BTreeSet;
+
     use crate::{ast::helpers::*, helpers::expect_ast};
 
     #[test]
@@ -83,5 +136,48 @@ mod tests {
         assert!(!expect_ast("(x + 1) * (x + 3)")
             .simplify()
             .is_polynomial_gpe(&[sym("x")]));
+    }
+
+    #[test]
+    fn gets_variables() {
+        assert_eq!(
+            expect_ast("x ^ 3 + 3 * x ^ 2 * y + 3 * x * y ^ 2 + y ^ 3")
+                .simplify()
+                .variables(),
+            BTreeSet::from([sym("x"), sym("y")])
+        );
+
+        assert_eq!(
+            expect_ast("3 * x * (x + 1) * y ^ 2 * z ^ n")
+                .simplify()
+                .variables(),
+            BTreeSet::from([
+                sym("x"),
+                sum([int(1), sym("x")]),
+                sym("y"),
+                pow(sym("z"), sym("n"))
+            ])
+        );
+
+        assert_eq!(
+            expect_ast("a * sin(x) ^ 2 + 2 * b * sin(x) + 3 * c")
+                .simplify()
+                .variables(),
+            BTreeSet::from([sym("a"), sym("b"), sym("c"), fun("sin", [sym("x")])])
+        );
+
+        assert_eq!(expect_ast("1/2").simplify().variables(), BTreeSet::new());
+
+        assert_eq!(
+            expect_ast("2 ^ (1/2) * x ^ 2 + 3 ^ (1/2) * x + 5 ^ (1/2)")
+                .simplify()
+                .variables(),
+            BTreeSet::from([
+                sym("x"),
+                pow(int(2), frc(1, 2)),
+                pow(int(3), frc(1, 2)),
+                pow(int(5), frc(1, 2)),
+            ])
+        );
     }
 }
